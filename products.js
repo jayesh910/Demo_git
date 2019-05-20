7e97efe4deaca21d978b046ec706c5a5cd618728
jayesh <jayeshpaw@cybage.com>
2019-05-20 13:24:56 +0530

    Gtronics first commit

---
+++ products.js
@@ -0,0 +1,18 @@
+var products = {
+    "name":"Toy1",
+    "price":"5400",
+    "name2":"Toy2",
+    "price2":"6400"
+}
+
+var nme = document.getElementById('prod1');
+nme.innerHTML = products.name;
+
+var price = document.getElementById('price1');
+price.innerHTML = products.price;
+
+var nme2 = document.getElementById('prod2');
+nme2.innerHTML = products.name2;
+
+var prce2 = document.getElementById('price2');
+prce2.innerHTML = products.price2;
